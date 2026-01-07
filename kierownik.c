1d15104c5dbd79f0dd651bda98308e2bf567bbb8
Wiksas <wiktor.sasnal04@gmail.com>
2026-01-07 18:45:14 +0100

    usuniecie polskich znakow w celu poprawengo wyswietlania sie projektu

--- kierownik.c
+++ kierownik.c
@@ -6,14 +6,14 @@ SharedData* sdata = NULL;
 void handle_sig1(int sig) {
     if (sdata) {
         sdata->kitchen_delay_us /= 2;
-        printf("\n[Kierownik] Polecenie: PRZYŒPIESZAMY WYDAWANIE DAÑ! (Aktualny delay: %d us)\n", sdata->kitchen_delay_us);
+        printf("\n[Kierownik] Polecenie: PRZYSPIESZAMY WYDAWANIE DAN! (Aktualny delay: %d us)\n", sdata->kitchen_delay_us);
     }
 }
 
 void handle_sig2(int sig) {
     if (sdata) {
         sdata->kitchen_delay_us *= 2;
-        printf("\n[Kierownik] Polecenie: ZWALNIAMY WYDAWANIE DAÑ! (Aktualny delay: %d us)\n", sdata->kitchen_delay_us);
+        printf("\n[Kierownik] Polecenie: ZWALNIAMY WYDAWANIE DAN! (Aktualny delay: %d us)\n", sdata->kitchen_delay_us);
     }
 }
 
@@ -22,19 +22,19 @@ void handle_sig3(int sig) {
         sdata->emergency_exit = true;
         sdata->open = false;
     }
-    printf("\n\033[1;31m[Kierownik] ALARM! EWAKUACJA I ZAMKNIÊCIE RESTAURACJI!\033[0m\n");
+    printf("\n\033[1;31m[Kierownik] ALARM! EWAKUACJA I ZAMKNIECIE RESTAURACJI!\033[0m\n");
 }
 
 int main() {
     int shmid = shmget(SHM_KEY, sizeof(SharedData), 0600);
     if (shmid == -1) {
-        perror("B³¹d shmget w kierownik.c");
+        perror("Blad shmget w kierownik.c");
         exit(1);
     }
 
     sdata = (SharedData*)shmat(shmid, NULL, 0);
     if (sdata == (void*)-1) {
-        perror("B³¹d shmat w kierownik.c");
+        perror("Blad shmat w kierownik.c");
         exit(1);
     }
 
@@ -42,7 +42,7 @@ int main() {
     signal(SIGUSR2, handle_sig2);
     signal(SIGALRM, handle_sig3);
 
-    printf("[KIEROWNIK] Zarz¹dca uruchomiony. PID: %d. Czekam na sygna³y (SIGUSR1, SIGUSR2, SIGALRM).\n", getpid());
+    printf("[KIEROWNIK] Zarzadca uruchomiony. PID: %d. Czekam na sygnaly (SIGUSR1, SIGUSR2, SIGALRM).\n", getpid());
 
     sdata->current_time = sdata->start_time;
 
@@ -60,7 +60,7 @@ int main() {
 
     if (!sdata->emergency_exit) {
         sdata->is_closed_for_new = true;
-        printf("[Kierownik] Godzina %02d:%02d osi¹gniêta. Zamykamy biletomat. Koñczymy obs³ugê obecnych goœci.\n",
+        printf("[Kierownik] Godzina %02d:%02d osiagnieta. Zamykamy biletomat. Konczymy obsluge obecnych gosci.\n",
             sdata->end_time / 60, sdata->end_time % 60);
     }
 
